147c1230315c7631bd1c3152cb5e9071661653aa
Nicholas Lum <nicholaslum444@hotmail.com>
2016-04-03 21:27:34 +0800

    Improved output and updated readme
    Program output should be more informative now.
    
    Readme contains compilation and usage instructions.

--- mifare-check-digital-signature.c
+++ mifare-check-digital-signature.c
@@ -1,4 +1,4 @@
-#include "config.h"
+//#include "config.h"
 
 #include <err.h>
 #include <errno.h>
@@ -28,13 +28,19 @@ bool check_digital_signature (FreefareTag freefaretag)
 //DUmmy function that returns the keys by checking the UID, should be replaced by NUS implementation
 void get_keys_from_uid_from_card (FreefareTag freefaretag, NUSkeys *nuskeys)
 {
-	MifareClassicKey keya = {0x5e, 0x87, 0x98, 0xec, 0x78, 0x0c};
+	//MifareClassicKey keya = {0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5}; // default
+	MifareClassicKey keya = {0xb3, 0x69, 0x17, 0x7a, 0xa3, 0x5f}; // nick-staff
+	//MifareClassicKey keya = {0x5e, 0x87, 0x98, 0xec, 0x78, 0x0c}; // leon
+
 	memcpy (&nuskeys->sector1keyA, &keya, 6);
+    //TODO if-else cases to get the uid between our cards
+    // i.e. if (LEON_UID) then keya = LEON_KEY_A
 }
 
 
 void usage (char *progname) {
 	//TODO if we have options
+    // one option can be inputting the key, since we don't have the lookup yet
 }
 
 int main(int argc, char *argv[])
@@ -86,6 +92,7 @@ int main(int argc, char *argv[])
 	    switch (freefare_get_tag_type (tags[i])) {
 		    case MIFARE_CLASSIC_1K:
 			break;
+
 		    default:
 			continue;
 	    }
@@ -98,26 +105,34 @@ int main(int argc, char *argv[])
 	    printf ("Found %s with UID %s. \n", tag_friendly_name, tag_uid);
 
 		if (mifare_classic_connect(tags[i]) < 0) {
-	    	printf ("Cannot connect to %s with UID %s", tag_friendly_name, tag_uid);
-		    free (tag_uid);
-		    free (tag_friendly_name);
-		    continue;
+	    	printf ("Cannot connect to %s with UID %s. \n", tag_friendly_name, tag_uid);
+            error = EXIT_FAILURE;
+            goto errorrr;
 	    }
 
+        printf ("Connected to card. \n");
+        printf ("Attempting to authenticate... \n");
+
 	    if (mifare_classic_authenticate (tags[i], 0x07, nuskeys.sector1keyA, MFC_KEY_A) < 0) {
-	    	printf ("Cannot authenticate with %s with UID %s", tag_friendly_name, tag_uid);
-		    free (tag_uid);
-		    free (tag_friendly_name);
-		    continue;
+	    	printf ("Cannot authenticate with %s with UID %s. \n", tag_friendly_name, tag_uid);
+            error = EXIT_FAILURE;
+            goto errorrr;
 	    }
+
+        printf ("Authentication successful. \n");
+        printf ("Reading card data... \n");
 
 	    //all relevant NUS information (that we know of) is stored in blocks 4 through 6 
 	    for (int j = 0; j < 3; j++) {
 		    if (mifare_classic_read (tags[i], 0x04 + j, &data[j]) < 0) {
-		    	printf ("Cannot read from block %d for %s with UID %s", 4+j, tag_friendly_name, tag_uid);
+		    	printf ("Cannot read from block %d for %s with UID %s. \n", 4+j, tag_friendly_name, tag_uid);
+                error = EXIT_FAILURE;
 			    goto errorrr;
 		    }
 	    }
+
+        printf ("Data read successful. \n");
+        printf ("Checking signature... \n");
 
 	    //we propose storing a digital signature at block 12, so this reads block 12
 	    // if (mifare_classic_read (tags[i], 0x40, &signature) < 0) {
@@ -126,16 +141,32 @@ int main(int argc, char *argv[])
 		   //  free (tag_friendly_name);
 		   //  continue;
 	    // }
+
+        printf ("Signature check done. \n");
+        printf ("Printing results... \n");
 	    
-	    printf ("The 5th block is :\n");
+	    printf ("The 5th block is : \n");
 	    for (int j = 0; j < 16; j++) {
 	    	printf ("%02x ", data[0][j]);
 	    }
+        printf ("\n");
 
-		errorrr:
-	    free (tag_uid);
-	    free (tag_friendly_name);
+        printf ("Results printed. \n");
+
+        errorrr:
+        if (error != EXIT_SUCCESS) {
+            printf ("Freeing tag uid pointer. \n");
+            free (tag_uid);
+            printf ("Freeing tag friendly name pointer. \n");
+            //free (tag_friendly_name);
+            // apparently this variable is not allocated by malloc,
+            // so it's inappropriate to use free() here.
+            // commenting it out so that it won't crash while running
+            printf ("Unable to free tag friendly name pointer. \n");
+        }
 	}
+
+    printf ("Completed. \n");
 
 	freefare_free_tags (tags);
 	nfc_close (device);
